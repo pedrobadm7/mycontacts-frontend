74634cea6ffabfa3eb65c27fa1ec87a43f8a5f26
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-03 13:29:39 -0300

    Criando input e select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,15 @@
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
+import Select from "../../components/Select";
 
 export default function NewContact() {
-    return <PageHeader title="Novo Contato" />;
+    return (
+        <>
+            <PageHeader title="Novo Contato" />
+            <Input type="text" placeholder="Nome" />
+            <Select>
+                <option value="123">Instagram</option>
+            </Select>
+        </>
+    );
 }
