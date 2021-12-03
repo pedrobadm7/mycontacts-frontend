a060bfce3ff556e488a3f58eb834e8f41d120572
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-03 10:19:36 -0300

    criando listagem de contatos

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,16 @@
 export default {
-    backgroundColor: "#F6F5FC",
+    colors: {
+        background: "#F6F5FC",
+        primary: {
+            lighter: "#E0E3FF",
+            light: "#6674F4",
+            main: "#5061FC",
+            dark: "#3346F0",
+        },
+        gray: {
+            900: "#222222",
+            200: "#BCBCBC",
+            100: "#E6E6E6",
+        },
+    },
 };
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "../../assets/styles/global";
 import defaultTheme from "../../assets/styles/themes/default";
+import ContactsList from "../ContactsList";
 import Header from "../Header";
 
 import * as S from "./styles";
@@ -12,6 +13,7 @@ function App() {
             <GlobalStyles />
             <S.Container>
                 <Header />
+                <ContactsList />
             </S.Container>
         </ThemeProvider>
     );
