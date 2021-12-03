bb2f4a64fae43aa7cb3712edea533004e31a6c86
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-03 11:12:35 -0300

    Criando a página Home e refatorando o Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,6 @@ export default function Header() {
     return (
         <S.Container>
             <img src={logo} alt="MyContacts" width="201" />
-
-            <S.InputSearchContainer>
-                <input type="text" placeholder="Pesquisar contato" />
-            </S.InputSearchContainer>
         </S.Container>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,65 @@
+import * as S from "./styles";
+
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
+
 export default function Home() {
-    return "Homepage";
+    return (
+        <S.Container>
+            <S.InputSearchContainer>
+                <input type="text" placeholder="Pesquisar contato" />
+            </S.InputSearchContainer>
+            <S.Header>
+                <strong>3 contatos</strong>
+                <a href="/">Novo Contato</a>
+            </S.Header>
+
+            <S.ListContainer>
+                <header>
+                    <button type="button">
+                        <span>Nome</span>
+                        <img src={arrow} alt="Arrow" />
+                    </button>
+                </header>
+
+                <S.Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>Pedro Barros</strong>
+                            <small>Instagram</small>
+                        </div>
+                        <span>pedrobars7a@gmail.com</span>
+                        <span>(73) 9 9957-7204</span>
+                    </div>
+                    <div className="actions">
+                        <a href="/">
+                            <img src={edit} alt="Edit" />
+                        </a>
+                        <button type="button">
+                            <img src={trash} alt="Delete" />
+                        </button>
+                    </div>
+                </S.Card>
+                <S.Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>Pedro Barros</strong>
+                            <small>Instagram</small>
+                        </div>
+                        <span>pedrobars7a@gmail.com</span>
+                        <span>(73) 9 9957-7204</span>
+                    </div>
+                    <div className="actions">
+                        <a href="/">
+                            <img src={edit} alt="Edit" />
+                        </a>
+                        <button type="button">
+                            <img src={trash} alt="Delete" />
+                        </button>
+                    </div>
+                </S.Card>
+            </S.ListContainer>
+        </S.Container>
+    );
 }
