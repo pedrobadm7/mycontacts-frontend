d7ad4ae31300181b2918a3552f8c99d58483c354
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-04 20:24:24 -0300

    Criação do componente button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from "../../components/Button";
 import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
 import Select from "../../components/Select";
@@ -10,6 +11,10 @@ export default function NewContact() {
             <Select>
                 <option value="123">Instagram</option>
             </Select>
+            <Button type="button">Salvar alterações</Button>
+            <Button type="button" disabled>
+                Salvar alterações
+            </Button>
         </>
     );
 }
