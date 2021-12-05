e1e067cb0c142fd461da204cdc206316757a8ba5
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-05 09:29:39 -0300

    Criando status danger no modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,31 @@
+import PropTypes from "prop-types";
 import * as S from "./styles";
 
 import Button from "../Button";
 
-export default function Modal() {
+export default function Modal({ danger }) {
     return (
         <S.Overlay>
-            <S.Container>
+            <S.Container danger={danger}>
                 <h1>Título do Modal</h1>
                 <p>Corpo do Modal</p>
                 <S.Footer>
                     <button type="button" className="cancel-button">
                         Cancelar
                     </button>
-                    <Button type="button">Deletar</Button>
+                    <Button type="button" danger={danger}>
+                        Deletar
+                    </Button>
                 </S.Footer>
             </S.Container>
         </S.Overlay>
     );
 }
+
+Modal.propTypes = {
+    danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+    danger: false,
+};
