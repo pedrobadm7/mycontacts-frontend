27fdbe1134bbfe1b2eb9e644e0041f6c32615c25
pedrobadm7 <pedrobars7a@gmail.com>
2022-01-01 11:03:27 -0300

    Melhorias de UX com status de loading

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import * as S from "./styles";
 
-export default function Loader() {
+export default function Loader({ isLoading }) {
+    if (!isLoading) return null;
+
     return ReactDOM.createPortal(
         <S.Overlay>
             <div className="loader" />
@@ -9,3 +12,7 @@ export default function Loader() {
         document.getElementById("loader-root")
     );
 }
+
+Loader.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,41 +1,100 @@
 import { Link } from "react-router-dom";
 
+import { useEffect, useState, useMemo } from "react";
 import * as S from "./styles";
 
 import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
 
+import Loader from "../../components/Loader";
+import delay from "../../utils/delay";
+
 export default function Home() {
+    const [contacts, setContacts] = useState([]);
+    const [orderBy, setOrderBy] = useState("asc");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+
+    const filteredContacts = useMemo(() => {
+        return contacts.filter((contact) => {
+            return contact.name
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase());
+        });
+    }, [contacts, searchTerm]);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
+            .then(async (response) => {
+                await delay(500);
+
+                const json = await response.json();
+                setContacts(json);
+            })
+            .catch((error) => {
+                console.log("error: ", error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, [orderBy]);
+
+    function handleToggleOrderBy() {
+        setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
+    }
+
+    function handleChangeSearchTerm(event) {
+        setSearchTerm(event.target.value);
+    }
+
     return (
         <S.Container>
+            <Loader isLoading={isLoading} />
+
             <S.InputSearchContainer>
-                <input type="text" placeholder="Pesquisar contato" />
+                <input
+                    value={searchTerm}
+                    type="text"
+                    placeholder="Pesquisar contato"
+                    onChange={handleChangeSearchTerm}
+                />
             </S.InputSearchContainer>
             <S.Header>
-                <strong>3 contatos</strong>
+                <strong>
+                    {filteredContacts.length}{" "}
+                    {filteredContacts.length === 1 ? "contato" : " contatos"}
+                </strong>
                 <Link to="/new">Novo Contato</Link>
             </S.Header>
 
-            <S.ListContainer>
-                <header>
-                    <button type="button">
+            {filteredContacts.length > 0 ? (
+                <S.ListHeader orderBy={orderBy}>
+                    <button type="button" onClick={handleToggleOrderBy}>
                         <span>Nome</span>
                         <img src={arrow} alt="Arrow" />
                     </button>
-                </header>
+                </S.ListHeader>
+            ) : (
+                <h3>Ops, nenhum resultado para essa busca...</h3>
+            )}
 
-                <S.Card>
+            {filteredContacts.map((contact) => (
+                <S.Card key={contact.id}>
                     <div className="info">
                         <div className="contact-name">
-                            <strong>Pedro Barros</strong>
-                            <small>Instagram</small>
+                            <strong>{contact.name}</strong>
+                            {contact.category_name && (
+                                <small>{contact.category_name}</small>
+                            )}
                         </div>
-                        <span>pedrobars7a@gmail.com</span>
-                        <span>(73) 9 9957-7204</span>
+                        <span>{contact.email}</span>
+                        <span>{contact.phone}</span>
                     </div>
                     <div className="actions">
-                        <Link to="/edit/123">
+                        <Link to={`/edit/${contact.id}`}>
                             <img src={edit} alt="Edit" />
                         </Link>
                         <button type="button">
@@ -43,15 +102,7 @@ export default function Home() {
                         </button>
                     </div>
                 </S.Card>
-            </S.ListContainer>
+            ))}
         </S.Container>
     );
 }
-
-fetch("http://localhost:3001/contacts")
-    .then((response) => {
-        console.log("response", response);
-    })
-    .catch((error) => {
-        console.log("erro", error);
-    });
