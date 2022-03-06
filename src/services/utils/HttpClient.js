200ab326f37fcfab8fbfea36e6b777273f86f458
Pedro Barros <pedrobars7a@gmail.com>
2022-03-05 21:34:16 -0300

    Revert "create PUT httpclient"
    This reverts commit 3d4747e2227d6baa9d46aa6bb9d4839ed89147a6.

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -40,28 +40,6 @@ class HttpClient {
         }
 
         if (response.ok) {
-            return body;
-        }
-
-        throw new APIError(response, responseBody);
-    }
-
-    async put(path, body) {
-        const response = await fetch(`${this.baseURL}${path}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-        });
-
-        let responseBody = null;
-        const contentType = response.headers.get("Content-Type");
-        if (contentType.includes("application/json")) {
-            responseBody = await response.json();
-        }
-
-        if (response.ok) {
             return responseBody;
         }
 
