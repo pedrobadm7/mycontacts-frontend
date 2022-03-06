419855b2368bab41de18afd06eceab21f00c4e87
Pedro Barros <pedrobars7a@gmail.com>
2022-03-05 23:45:17 -0300

    Adicionando status de loading no Select de categorias

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -19,6 +19,7 @@ export default function ContactForm({ buttonLabel }) {
     const [phone, setPhone] = useState("");
     const [categoryId, setCategoryId] = useState("");
     const [categories, setCategories] = useState([]);
+    const [isLoadingCategories, setIsLoadingCategories] = useState(true);
 
     const { setError, removeError, getErrorMessageByFieldName, errors } =
         useErrors();
@@ -31,7 +32,10 @@ export default function ContactForm({ buttonLabel }) {
                 const categoriesList = await CategoriesService.listCategories();
 
                 setCategories(categoriesList);
-            } catch {}
+            } catch {
+            } finally {
+                setIsLoadingCategories(false);
+            }
         }
 
         loadCategories();
@@ -104,10 +108,11 @@ export default function ContactForm({ buttonLabel }) {
                 />
             </FormGroup>
 
-            <FormGroup>
+            <FormGroup isLoading={isLoadingCategories}>
                 <Select
                     value={categoryId}
                     onChange={(e) => setCategoryId(e.target.value)}
+                    disabled={isLoadingCategories}
                 >
                     <option value="">Sem Categoria</option>
                     {categories.map((category) => (
