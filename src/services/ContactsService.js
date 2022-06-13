37052a173f4b022005f9b2f1c05a1fe03319a26e
Pedro Barros <pedrobars7a@gmail.com>
2022-06-13 19:27:34 -0300

    fix

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -6,11 +6,7 @@ class ContactsService {
     }
 
     async listContacts(orderBy = "asc") {
-        return this.httpClient.get(`/contacts?orderBy=${orderBy}`, {
-            headers: {
-                Authorization: "meutoken",
-            },
-        });
+        return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
     }
 
     async createContact(contact) {
