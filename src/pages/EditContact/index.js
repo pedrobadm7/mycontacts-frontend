b97a1e0dee12611e58f7bd27fd0be8524557a391
Pedro Barros <pedrobars7a@gmail.com>
2022-07-18 19:15:53 -0300

    Carregando os dados do contato na página de edição

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,11 +1,45 @@
+import { useHistory, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
+import ContactsService from "../../services/ContactsService";
+import Loader from "../../components/Loader";
+import toast from "../../utils/toast";
 
 export default function EditContact() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { id } = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        async function loadContact() {
+            try {
+                const contactData = await ContactsService.getContactById(id);
+                setIsLoading(false);
+                console.log({ contactData });
+            } catch {
+                history.push("/");
+                toast({
+                    type: "danger",
+                    text: "Contato não encontrado",
+                });
+            }
+        }
+
+        loadContact();
+    }, [id, history]);
+
+    function handleSubmit() {}
+
     return (
         <>
-            <PageHeader title="Editar Pedro Barros" />
-            <ContactForm buttonLabel="Salvar alterações" />
+            <Loader isLoading={isLoading} />
+            <PageHeader title="Editar " />
+            <ContactForm
+                buttonLabel="Salvar alterações"
+                onSubmit={handleSubmit}
+            />
         </>
     );
 }
